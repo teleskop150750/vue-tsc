9730d6a91cb2d6e323d050d589aa3ff16019c129
Валентин Степанов <teleskop150750@gmail.com>
2023-07-25 22:05:17 +0300

    first commit

---
+++ packages/ui-kit-constants/src/NButton/NButton.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'NButton'
+})
+
+defineProps<{
+  text: string;
+}>();
+
+const emit = defineEmits<{
+  (e: 'click', event: MouseEvent): void
+}>();
+
+function handleCLick(event: MouseEvent) {
+  emit('click', event);
+}
+</script>
+
+<template>
+  <button type="button" @click="handleCLick">
+    {{ text }}
+  </button>
+</template>
---
+++ play/vue/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { NButton } from '@nado/ui-kit-components';
+</script>
+
+<template>
+  <div class="n-box">
+    <NButton text="button" />
+  </div>
+</template>
